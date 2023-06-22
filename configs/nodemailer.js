f8c9742deb24d85450e10b1f9f710c810db9b088
Daria Khrushch <73955564+Daria-Khrushch@users.noreply.github.com>
2023-06-22 10:42:10 +0100

    add env

--- configs/nodemailer.js
+++ configs/nodemailer.js
@@ -1,12 +1,8 @@
 import nodemailer from "nodemailer";
 
-// const email = process.env.EMAIL;
-// const emailTo = process.env.EMAIL_TWO;
-// const pass = process.env.EMAIL_PASS;
-
-const email = "dariakh.test@gmail.com";
-const emailTo = "daria21khrushch@gmail.com";
-const pass = "yduigurcsaizdxcp";
+const email = process.env.EMAIL;
+const emailTo = process.env.EMAIL_TWO;
+const pass = process.env.EMAIL_PASS;
 
 export const transporter = nodemailer.createTransport({
   service: "gmail",
